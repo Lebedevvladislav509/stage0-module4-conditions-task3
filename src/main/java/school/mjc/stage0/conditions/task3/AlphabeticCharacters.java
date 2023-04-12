48a1536df5143357be676b2a9b3a6612f1d7e55d
LVLAD2009@yandex.ru <24233lebedko>
2023-04-12 21:55:07 +0300

    stage0-module4-conditions-task3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        character = Character.toLowerCase(character);
+        if (character == 97 || character == 101 || character == 105 || character == 111 || character == 117 || character == 121) {
+            System.out.println("Vowel");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if ((symbol > 64 && symbol < 91) || (symbol > 96 && symbol < 122)) {
+            System.out.println("English");
+        }
+        System.out.println("Non English");
     }
 }
